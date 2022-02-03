62e50943aaa249f7d1d6ad0547a685bb83167545
josephsalazar <jsalazar13@att.net>
2022-02-03 12:25:53 -0800

    My solution for module 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(phrase, num) {
+    let  returnPhrase; 
+    returnPhrase = "";
 
+    if(num <0){
+        return "ERROR"; 
+    }else{
+        for (let i = 0; i<num; i++){
+            returnPhrase+=phrase; 
+        }
+        return returnPhrase;
+    } 
 };
 
 // Do not edit below this line
