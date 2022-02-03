e6d5d13b5145aeb79fcda0650b7dd42c35eb77a4
josephsalazar <jsalazar13@att.net>
2022-02-03 14:08:19 -0800

    My solution for exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArr, ...numArr) {
 
-};
+        for(let i = numArr.length - 1; i >= 0; i--){
+            let index = mainArr.indexOf(numArr[i]);
+    
+            if(index == -1){
+                continue;
+            }
+            else{
+                mainArr.splice(index, 1);
+            }
+    
+        }
+    
+      return mainArr; 
+    
+    }; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
